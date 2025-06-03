db3d8f4c543c48b130ddff175b060176ca571e7c
Michael Davis <mcarsondavis@gmail.com>
2025-06-03 10:08:17 -0400

    Set enableDiagnostics in prisma-language-server config
    Without this key `prisma-language-server` currently crashes during
    initialization. See #13662.

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -27,7 +27,7 @@ use std::{
     borrow::Cow,
     cell::Cell,
     collections::{BTreeMap, HashMap, HashSet},
-    fs,
+    env, fs,
     io::{self, stdin},
     num::{NonZeroU8, NonZeroUsize},
     path::{Path, PathBuf},
@@ -622,6 +622,9 @@ pub enum StatusLineElement {
 
     /// Indicator for selected register
     Register,
+
+    /// NL: Remove after debugging
+    DebugViewID,
 }
 
 // Cursor shape is read and used on every rendered frame and so needs
@@ -1955,6 +1958,9 @@ impl Editor {
 
     pub fn focus(&mut self, view_id: ViewId) {
         let prev_id = std::mem::replace(&mut self.tree.focus, view_id);
+        let curr_id = self.tree.focus;
+        log::info!("id={:?}", curr_id);
+        env::set_var("HELIX_FOCUS_ID", format!("{:?}", curr_id));
 
         // if leaving the view: mode should reset and the cursor should be
         // within view
@@ -1989,7 +1995,9 @@ impl Editor {
     pub fn focus_direction(&mut self, direction: tree::Direction) {
         let current_view = self.tree.focus;
         if let Some(id) = self.tree.find_split_in_direction(current_view, direction) {
-            self.focus(id)
+            self.focus(id);
+        } else {
+            log::info!("We should forward to tmux!");
         }
     }
 
